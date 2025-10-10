1467489c50c432efeece6ef6ef004b54adb76280
Adham Ayad <adhamayad@localhost.localdomain>
2025-10-10 20:58:09 +0300

    setup Terraform project structure, backend, and deploy workflow

---
+++ Terraform/variables.tf
@@ -0,0 +1,11 @@
+variable "project_id" {
+       type = string
+       description = "Google Cloud project ID"
+       default = "konecta-task-467513"
+    }
+
+variable "region" {
+       type = string
+       description = "Current Region"
+       default = "us-east1"
+    }
