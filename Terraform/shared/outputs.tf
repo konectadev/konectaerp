9043519055009f0f3bc17dad8645c657870597c0
sherifemad53 <sherifemad53@gmail.com>
2025-11-20 00:38:25 +0200

    feat: Add new outputs for REPO_URL, RABBITMQ_URI, MAILHOG_URI, CONSUL_URI, and region, and refactor existing

--- Terraform/shared/outputs.tf
+++ Terraform/shared/outputs.tf
@@ -1,12 +1,13 @@
 output "service_account_email" { value = module.service_account.email }
 output "vpc_connector_name" { value = google_vpc_access_connector.serverless_connector.name }
 output "repo_url" { value = module.artifact_repo.url }
-output "url" { value = module.rabbitmq.uri }
+output "REPO_URL" { value = module.artifact_repo.url }
+output "RABBITMQ_URI" { value = module.rabbitmq.uri }
+output "MAILHOG_URI" { value = module.mailhog.uri }
+output "CONSUL_URI" { value = module.consul.uri }
 output "project_id" {
   value = var.project_id
 }
-
-output "CONSUL_URI" {
-  value = module.consul.uri
+output "region" {
+  value = var.region
 }
-
